16eab7e2fab34a66e3e9a5785c238f323bb26dc5
yonni <yonni.m@zend.com>
2014-09-03 09:46:39 +0300

    Added config Added module contents Moved around paths and classes

--- Module.php
+++ Module.php
@@ -1,11 +1,27 @@
 <?php
+namespace ZRay\Extension;
 use DevBar\ModuleManager\Feature\DevBarProducerProviderInterface;
 use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
-class Module implements DevBarProducerProviderInterface {
+class Module implements DevBarProducerProviderInterface, AutoloaderProviderInterface {
 	
 	public function getDevBarProducers(EventInterface $e) {
 		return array();
 	}
+	
+	/* (non-PHPdoc)
+	 * @see \Zend\ModuleManager\Feature\AutoloaderProviderInterface::getAutoloaderConfig()
+	 */
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\ClassMapAutoloader' => array(
+			    array(array(
+			    	'ZRay\Extension\Producer\Magento' => 'ZRay\Extension\Producer\Magento.php'
+			    ))
+			)
+		);
+	}
+
 }
 
